1596829198f6847a2d366b93943c8fad3d50ae21
RoyEden <roy.ede@gmail.com>
2021-11-23 06:19:35 -0300

    Added arrayShuffle as a util

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 export * from "./types";
 export * from "./components";
 export * from "./lib/hooks";
+export * from "./lib/utils";
---
+++ lib/lib/utils/index.d.ts
@@ -0,0 +1,1 @@
+export { default as arrayShuffle } from "./arrayShuffle";
